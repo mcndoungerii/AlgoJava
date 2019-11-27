d400b05eb066953a295601fb90be39718c12a037
Allen <allenmcndounger@gmail.com>
2019-11-27 20:20:50 +0300

    Bubble Sort

---
+++ program.java
@@ -0,0 +1,34 @@
+public class program {
+
+    static void bubbleSort(int[] arr) {
+        boolean swapped = true;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false) {
+                break;
+            }
+        }
+
+    }
+
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 42, 6, 1, 3, 2 };
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
